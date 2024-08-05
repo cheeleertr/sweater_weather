8df1a9e5247a6adba856763e60f47cf97ffda29c
Chee Lee <cheeleertr@gmail.com>
2024-08-04 21:38:58 -0500

    add user create endpoint

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,21 +1,18 @@
 class Api::V1::UsersController < ApplicationController
   def create
-    binding.pry
     user = User.new(user_params)
     if user.save
-        session[:user_id] = user.id
-        flash[:success] = "Welcome, #{user.name}!"
-        
+      user.api_keys.create!(token: SecureRandom.hex)
+      render json: UserSerializer.format_user(user), status: :created
     else
-      flash[:error] = "#{error_message(user.errors)}"
-      
+      render json: ErrorSerializer.new(ErrorMessage.new(user.errors.full_messages, 422)).serialize_json, status: :unprocessable_entity
     end
   end
 
 private
 
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation)
+    params.permit(:email, :password, :password_confirmation)
   end
 
 end
